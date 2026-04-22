753461873b70bee9e2c0da53c74ef8bee8abf64d
David Rynn <davidrynn@gmail.com>
2026-04-22 07:27:12 -0400

    initial commit

---
+++ CurrencyXchangeCalc/CurrencyXchangeCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyXchangeCalcApp.swift
+//  CurrencyXchangeCalc
+//
+//  Created by David Rynn on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyXchangeCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CurrencyXchangeCalcTests/CurrencyXchangeCalcTests.swift
@@ -0,0 +1,19 @@
+//
+//  CurrencyXchangeCalcTests.swift
+//  CurrencyXchangeCalcTests
+//
+//  Created by David Rynn on 4/22/26.
+//
+
+import Testing
+@testable import CurrencyXchangeCalc
+
+struct CurrencyXchangeCalcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
